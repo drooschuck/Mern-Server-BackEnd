c12a34f48584b829a492c2fdb1a10f6497abb8c3
drooschuck <114895991+drooschuck@users.noreply.github.com>
2024-09-27 23:51:57 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use("/api/reports", resportsRoute);
 
 const port = process.env.PORT || 5000;
 
-const path = require("path");
+/*const path = require("path");
 __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });   
-} 
+} */
 
 
 app.listen(port, () => {
