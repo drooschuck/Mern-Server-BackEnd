eb0687ac50ea2803e58f61bd7358e43d88b797bc
drooschuck <114895991+drooschuck@users.noreply.github.com>
2024-09-30 00:30:18 +0100

    Update server.js
    update 6th

--- server/server.js
+++ server/server.js
@@ -16,6 +16,14 @@ const authMiddleware = require("./middlewares/authMiddleware") */
 app.use("/api/exams", authMiddleware, examsRoute);
 app.use("/api/reports", authMiddleware, resportsRoute); */
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: ["https://mern-frontend-dn3i.onrender.com"],
+  })
+);
+
 app.use("/api/users", usersRoute);
 app.use("/api/exams", examsRoute);
 app.use("/api/reports", resportsRoute); 
